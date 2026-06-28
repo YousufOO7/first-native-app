276bd193f1d5eb218dee2017239ed95447f39cfc
Yousuf007 <yousufjobaer@gmail.com>
2026-06-29 01:30:51 +0600

    use the clerk google

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,44 +1,69 @@
 import { useAuth, useSignUp } from '@clerk/expo';
 import FormInput from 'app/utils/common/FormInput';
-import { useRouter } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 import { useState } from 'react';
-import { ActivityIndicator, ScrollView, Text,  TouchableOpacity,  View } from 'react-native';
+import { ActivityIndicator, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 export default function SignUp() {
-  const {signUp, fetchStatus} = useSignUp();
-  const {isSignedIn} = useAuth();
+  const { signUp, fetchStatus, errors } = useSignUp();
+  const { isSignedIn } = useAuth();
   const router = useRouter();
-  const isLoading = fetchStatus === "fetching";
-   const [firstName, setFirstName] = useState("");
-   const [lastName, setLastName] = useState("");
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
+  const isLoading = fetchStatus === 'fetching';
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+
+  const onSignUpPress = async () => {
+  const {error} = await signUp.password({
+      emailAddress: email,
+      password,
+      firstName,
+      lastName,
+    })
+
+    if (error) {
+      alert(`Error: ${error.message}`);
+    }
+
+    if(!error) await signUp.verifications.sendEmailCode();
+  }
+
+  if(
+    signUp.status === "missing_requirements" &&
+    signUp.unverifiedFields.includes("email_address") &&
+    signUp.missingFields.length === 0
+  ) {
+    return (
+      <View className="flex-1 justify-center items-center bg-white px-4">
+        <Text className="text-3xl font-bold">Check your email</Text>
+        <Text className="mt-4 text-lg font-semibold text-gray-700">
+          We&apos;ve sent a verification code to {email}. Please check your inbox and follow the instructions to verify your email address.
+        </Text>
+      </View>
+    )
+  }
 
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       className="bg-white"
       keyboardShouldPersistTaps="handled">
-      <View className="flex-1 bg-white px-4 justify-center">
+      <View className="flex-1 justify-center bg-white px-4">
         <Text className="text-3xl font-bold">SignUp</Text>
-        <Text className="text-xl text-gray-700 mt-4 font-semibold">Create your account</Text>
-        <Text className="text-lg text-gray-700 mt-1 font-semibold">Find your dream home today!</Text>
+        <Text className="mt-4 text-xl font-semibold text-gray-700">Create your account</Text>
+        <Text className="mt-1 text-lg font-semibold text-gray-700">
+          Find your dream home today!
+        </Text>
 
         {/* input fields names */}
-        <View className="flex-row gap-3 my-4">
+        <View className="my-4 flex-row gap-3">
           <FormInput
             label="First Name"
             placeholder="Enter first name"
             value={firstName}
             onChangeText={setFirstName}
-            error={errors.firstName}
             textContentType="givenName"
             autoCapitalize="words"
           />
@@ -47,49 +72,56 @@ export default function SignUp() {
             placeholder="Enter last name"
             value={lastName}
             onChangeText={setLastName}
-            error={errors.lastName}
             textContentType="familyName"
             autoCapitalize="words"
           />
         </View>
 
         {/* input fields email and password */}
+        <View>
           <FormInput
             label="Email"
             placeholder="Enter your email"
             value={email}
             onChangeText={setEmail}
-            error={errors.email}
             keyboardType="email-address"
             textContentType="emailAddress"
             autoCapitalize="none"
           />
+          {errors.fields.emailAddress && (
+            <Text className="text-red-500 text-sm mt-1">
+              {errors.fields.emailAddress.message}
+            </Text>
+          )}
           <FormInput
             label="Password"
             placeholder="Enter your password"
             value={password}
             onChangeText={setPassword}
-            error={errors.password}
             secureTextEntry={true}
           />
+          {errors.fields.password && (
+            <Text className="text-red-500 text-sm mt-1">
+              {errors.fields.password.message}
+            </Text>
+          )}
 
           <TouchableOpacity
-            className="bg-blue-500 py-3 rounded-lg mt-4"
-            // onPress={handleSignUp}
+            className="mt-4 rounded-lg bg-blue-500 py-3"
+            onPress={onSignUpPress}
           >
-            <Text className="text-white text-center text-lg font-semibold">
-              {
-                isLoading ? <ActivityIndicator /> : "Sign Up"
-              }
+            <Text className="text-center text-lg font-semibold text-white">
+              {isLoading ? <ActivityIndicator color="white" /> : 'Sign Up'}
             </Text>
           </TouchableOpacity>
 
-          <View className="flex-row justify-center mt-4">
+          <View className="mt-4 flex-row justify-center">
             <Text className="text-gray-700">Already have an account? </Text>
-            <TouchableOpacity onPress={() => router.push("/(auth)/sign-in")}>
-              <Text className="text-blue-500 font-semibold">Sign In</Text>
-            </TouchableOpacity>
+            <Link href="/sign-in">
+              <Text className="font-semibold text-blue-500">Sign In</Text>
+            </Link>
           </View>
+        </View>
       </View>
     </ScrollView>
   );
--- app/utils/common/FormInput.tsx
+++ app/utils/common/FormInput.tsx
@@ -3,8 +3,8 @@ import { Text, TextInput, View } from "react-native";
 interface FormInputProps {
   label?: string;
   placeholder: string;
-  value?: string;
-  onChangeText?: (text: string) => void;
+  value: string;
+  onChangeText: (text: string) => void;
   error?: string;
   keyboardType?:
     | "default"
