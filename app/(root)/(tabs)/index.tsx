4c2fd1cfee9e2284b32f8426ac6ce7bb28d3db36
Yousuf007 <yousufjobaer@gmail.com>
2026-07-02 14:03:04 +0600

    fetch the data from supabase and show it

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,133 @@
-import { Text, View } from "react-native";
+import { useUser } from "@clerk/expo";
+import { Ionicons } from "@expo/vector-icons";
+import { Property } from "app/utils/types/property";
+import { useFocusEffect, useRouter } from "expo-router";
+import { supabase } from "lib/supabase";
+import { useCallback, useState } from "react";
+import { ActivityIndicator, FlatList, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 
 export default function HomeScreen() {
+    const { user } = useUser();
+  const router = useRouter();
+
+  const [featured, setFeatured] = useState<Property[]>([]);
+  const [recommended, setRecommended] = useState<Property[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  console.log(featured, recommended);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchProperties();
+    }, [])
+  );
+
+  const fetchProperties = async () => {
+    setLoading(true);
+
+    const { data: featuredData } = await supabase
+      .from("properties")
+      .select("*")
+      .eq("is_featured", true)
+      .order("created_at", { ascending: false });
+
+    const { data: recommendedData } = await supabase
+      .from("properties")
+      .select("*")
+      .eq("is_featured", false)
+      .order("created_at", { ascending: false });
+
+    setFeatured(featuredData ?? []);
+    setRecommended(recommendedData ?? []);
+    setLoading(false);
+  };
+
   return (
-    <View className="flex-1 bg-white">
-      <Text className="text-3xl">HomeScreen</Text>
-    </View>
+      <SafeAreaView className="flex-1 bg-gray-50">
+      <FlatList
+        data={recommended}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View>
+            {/* Header */}
+            <View className="flex-row items-center justify-between px-5 pt-4 pb-5">
+              <Text className="text-lg font-bold">Native App</Text>
+              <View className="items-end">
+                <Text className="text-gray-500 text-xs">Good morning 👋</Text>
+                <Text className="text-gray-900 text-base font-bold">
+                  {user?.firstName ?? "User"}
+                </Text>
+              </View>
+            </View>
+
+            {/* Search Bar */}
+            <TouchableOpacity
+              onPress={() => router.push("/(root)/(tabs)/search")}
+              className="mx-5 mb-6 flex-row items-center bg-white rounded-2xl px-4 py-3 gap-3"
+              
+            >
+              <Ionicons name="search-outline" size={18} color="#9CA3AF" />
+              <Text className="text-gray-400 text-sm flex-1">
+                Search properties, cities...
+              </Text>
+              <TouchableOpacity
+                onPress={() =>
+                  router.push("/(root)/(tabs)/search?openFilters=true")
+                }
+                className="w-8 h-8 bg-blue-600 rounded-xl items-center justify-center"
+              >
+                <Ionicons name="options-outline" size={15} color="white" />
+              </TouchableOpacity>
+            </TouchableOpacity>
+
+            {/* Featured Section */}
+            <View className="px-5 mb-6">
+                <Text className="font-bold text-lg mb-6">Featured</Text>
+
+                {
+              loading ? (
+                <ActivityIndicator size="small" color="#0000ff" className="py-10" />
+              ) : (
+                <FlatList
+                data={featured}
+                keyExtractor={(item) => item.id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => (
+                  <View className="bg-white rounded-2xl mx-1 mb-6 p-1">
+                    <Text className="text-gray-900 text-base font-bold">
+                      {item.title}
+                    </Text>
+                  </View>
+                )}
+                >
+
+                </FlatList>
+              )
+            }
+            </View>
+
+            {/* Recommended Header */}
+            <Text className="text-gray-900 text-lg font-bold px-5 mb-4">
+              Recommended
+            </Text>
+          </View>
+        }
+        renderItem={({ item }) => (
+          <Text>
+            {item.title}
+          </Text>
+        )}
+        ListEmptyComponent={
+          !loading ? (
+            <View className="items-center py-10">
+              <Text className="text-gray-400">No properties found</Text>
+            </View>
+          ) : null
+        }
+      />
+    </SafeAreaView>
   );
 }
